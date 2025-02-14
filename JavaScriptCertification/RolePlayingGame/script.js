8b54676896078b3dc75c381500d3cf860529785f
Sillysally <92034603+SallyResch@users.noreply.github.com>
2025-02-14 14:34:35 +0100

    added link to java scrpit project in profile. added logics for selling weapon

--- JavaScriptCertification/RolePlayingGame/script.js
+++ JavaScriptCertification/RolePlayingGame/script.js
@@ -1,6 +1,6 @@
 let xp = 0;
 let health = 100;
-let gold = 250;
+let gold = 50;
 let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
@@ -22,6 +22,7 @@ const weapons = [
   { name: 'claw hammer', power: 50 },
   { name: 'sword', power: 100 }
 ];
+
 const locations = [
   {
     name: "town square",
@@ -70,10 +71,6 @@ function goCave() {
   update(locations[2]);
 }
 
-function fightDragon() {
-  console.log("Fighting dragon.");
-}
-
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
@@ -100,9 +97,22 @@ function buyWeapon() {
     }
   } else {
     text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
   }
 }
 
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
+  }
+}
 
 function fightSlime() {
 
@@ -110,5 +120,9 @@ function fightSlime() {
 
 function fightBeast() {
 
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
 }
 
