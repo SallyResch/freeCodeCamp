7e201c1732143d42900161deef21a46b88789d3b
Sillysally <92034603+SallyResch@users.noreply.github.com>
2025-02-19 14:29:24 +0100

    added function getRoundResults

--- JavaScriptCertification/RockPaperScissors/script.js
+++ JavaScriptCertification/RockPaperScissors/script.js
@@ -1,5 +1,30 @@
 function getRandomComputerResult() {
-    const options = ["Rock", "Paper", "Scissors"];
-  
+  const options = ["Rock", "Paper", "Scissors"];
+  const optionIndex = Math.floor(Math.random() * options.length);
+  return options[optionIndex];
+}
+
+function hasPlayerWonTheRound(player, computer) {
+  if((player === "Rock" && computer === "Scissors")|| (player === "Scissors" && computer === "Paper") || (player === "Paper" && computer === "Rock")){
+    return true;
+  }else{
+    return false;
   }
-  console.log(getRandomComputerResult());
+}
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+
+  if(hasPlayerWonTheRound === true){
+    const playerWon = "Player wins! "+ userOption +" beats " + computerResult;
+    playerScore += 1;
+    return playerWon;
+  }else if(hasPlayerWonTheRound === false){
+    const computerWon = "Computer wins! "+ computerResult +" beats " + userOption;
+    computerScore += 1;
+    return computerWon;
+  }else{
+    const tieResult = "It's a tie! Both chose " + userOption;
+    return tieResult;
+  }
+}
